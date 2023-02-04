12844bddd6aa10eae8fc473c2cc567dddfa37357
Thomas Kam <thomassth@users.noreply.github.com>
2023-02-03 23:44:16 -0500

    day1.1

--- exercises/03_if/if2.rs
+++ exercises/03_if/if2.rs
@@ -1,9 +1,12 @@
 // TODO: Fix the compiler error on this function.
-fn picky_eater(food: &str) -> &str {
+
+pub fn picky_eater(food: &str) -> &str {
     if food == "strawberry" {
-        "Yummy!"
+        "Yummy"
+    } else if food == "potato" {
+        "I guess I can eat that."
     } else {
-        1
+        "No thanks!"
     }
 }
 
