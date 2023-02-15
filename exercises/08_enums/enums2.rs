53c1d38b6569b55da9cbdb101a127e9854769b4c
Thomas Kam <thomassth@users.noreply.github.com>
2023-02-14 21:58:34 -0500

    enums and strings

--- exercises/08_enums/enums2.rs
+++ exercises/08_enums/enums2.rs
@@ -6,7 +6,10 @@ struct Point {
 
 #[derive(Debug)]
 enum Message {
-    // TODO: Define the different variants used below.
+    Move { x: i32, y: i32 },
+    Echo(String),
+    ChangeColor(i32, i32, i32),
+    Quit, // TODO: Define the different variants used below.
 }
 
 impl Message {
